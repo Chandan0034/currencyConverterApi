128dddd4898c353ced0aaffca02c2c9cbe7122e1
Chandan kumar <chandandpu@gmail.com>
2024-02-13 10:53:56 +0530

    modified

--- app.js
+++ app.js
@@ -8,7 +8,9 @@ app.use(cors())
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(express.json());
 const PORT=process.env.PORT||4000;
-let output;
+let output=[];
+let currencyConvert=new CC();
+output=currencyConvert.currencyCode;
 app.get('/',async(req,res)=>{
     return res.status(200).send("Hello Welcome To Currency Converter App")
 })
@@ -16,31 +18,46 @@ app.get('/api/converter',async(req,res)=>{
     try {
         const {source,target,value}=req.query;
         let currencyConvert=new CC();
-        currencyConvert.from(source).to(target).convert(parseFloat(value)).then((response)=>{
-           if(response){
-            return res.status(200).json({
-                success:true,
-                sourse:source,
-                target:target,
-                value:value,
-                convetedAmount:response
-            })
-           }else{
-            return res.status(400).json({
+        if(output.includes(source.toUpperCase()) && output.includes(target.toUpperCase())){
+            currencyConvert.from(source).to(target).convert(parseFloat(value)).then((response)=>{
+                if(response){
+                 return res.status(200).json({
+                     success:true,
+                     sourse:source,
+                     target:target,
+                     value:value,
+                     convetedAmount:response
+                 })
+                }else{
+                 return res.status(400).json({
+                     success:false,
+                     sourse:source,
+                     target:target,
+                     value:value,
+                     convetedAmount:"Conveted Error"
+                 })
+                }
+             }).catch((error)=>{
+                 return res.status(500).json({
+                     success:false,
+                     sourse:source,
+                     target:target,
+                     value:value,
+                     convetedAmount:error
+                 })
+             })
+        }else{
+            return res.status(501).json({
                 success:false,
                 sourse:source,
                 target:target,
                 value:value,
-                convetedAmount:"Conveted Error"
+                convetedAmount:"Please Write The Suitable Symbol"
             })
-           }
-        })
+        }
     } catch (error) {
         return res.status(401).json({
             success:false,
-            sourse:source,
-            target:target,
-            value:value,
             convetedAmount:error
         })
     }
